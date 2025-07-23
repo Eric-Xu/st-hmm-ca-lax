370ac04af4bf1da282093294d9e2d36ea34a2a9c
Eric Xu <exu.data@gmail.com>
2025-07-23 13:25:21 -0400

    Add streamlit files.

---
+++ constants/file.py
@@ -0,0 +1,30 @@
+# --- APP ASSETS ---
+CSS_DIR = "css"
+
+CSS_FILE = "styles.css"
+
+
+# --- APP PAGES ---
+PAGE_DIR = "views"
+
+BORROWER_LENDERS_PAGE_FILE = "borrower_lenders_page.py"
+BORROWER_LOANS_PAGE_FILE = "borrower_loans_page.py"
+BORROWER_TIMELINE_PAGE_FILE = "borrower_timeline_page.py"
+LENDER_APPEAL_PAGE_FILE = "lender_appeal_page.py"
+LENDER_BORROWER_MIGRATION_PAGE_FILE = "lender_borrower_migration_page.py"
+LENDER_CHURNED_BORROWERS_PAGE_FILE = "lender_churned_borrowers_page.py"
+LENDER_MARKET_SHARE_PAGE_FILE = "lender_market_share_page.py"
+LENDER_ORIGINATION_TIMELINE_PAGE_FILE = "lender_origination_timeline_page.py"
+LENDER_REPEAT_BORROWERS_PAGE_FILE = "lender_repeat_borrowers_page.py"
+LOAN_ANALYSIS_PAGE_FILE = "loan_analysis_page.py"
+
+
+# --- DATA FILES ---
+DATA_DIR = "data"
+
+BORROWER_ACTIVITIES_DATA_FILE = "ca_la_20240701_20250630.csv"
+
+# --- TEMP FILES ---
+TMP_DIR = "tmp"
+
+TMP_DATA_JSON = "tmp_data.json"
